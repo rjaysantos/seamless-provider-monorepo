9efae1c05c6b286fac927279ae6c173b745f42de
Ralf-GA <ralf.it@goldenarrowsolution.com>
2025-06-17 09:21:04 +0800

    fixed date time for Red bonus API

--- Red/DTO/RedTransactionDTO.php
+++ Red/DTO/RedTransactionDTO.php
@@ -61,10 +61,7 @@ class RedTransactionDTO extends TransactionDTO
             currency: $playerDTO->currency,
             gameID: $requestDTO->gameID,
             betWinlose: $requestDTO->amount,
-            dateTime: self::convertProviderDateTime(
-                dateTime: Carbon::now(),
-                providerTimezone: self::PROVIDER_API_TIMEZONE
-            ),
+            dateTime: Carbon::now()->format('Y-m-d H:i:s'),
             winAmount: $requestDTO->amount
         );
     }
